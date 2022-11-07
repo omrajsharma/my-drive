0a1d0cddf71821a128df3e4056865f1a5f7df511
Omraj <omraj7413@gmail.com>
2022-11-07 13:50:28 +0530

    initial setup

---
+++ src/firebase.config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDW-uxNTUnORO2pYjaAMr8gvLGpjkBt7OE",
+    authDomain: "akash-learning-b6cb5.firebaseapp.com",
+    projectId: "akash-learning-b6cb5",
+    storageBucket: "akash-learning-b6cb5.appspot.com",
+    messagingSenderId: "36094450491",
+    appId: "1:36094450491:web:51aef6cda1a36cd0d2351e",
+    measurementId: "G-95H9JGP53B"
+};
+
